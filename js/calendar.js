001dabc82e725b41507574a0d6ac4dc4b95b412a
Chandan Mishra <bapunchandan009@gmail.com>
2021-09-03 14:55:33 +0530

    Fix previous month date

--- js/calendar.js
+++ js/calendar.js
@@ -1,6 +1,6 @@
 var today = new Date();
 today.setDate(1);
-var preDays = today.getDate();
+var preDays = today.getDay();
 var endDate = new Date(
   today.getFullYear(),
   today.getMonth() + 1, 0
@@ -10,13 +10,18 @@ var nextMonths = new Date(
   today.getMonth() + 1, 0
 ).getDay();
 var a = 7 - nextMonths - 1;
+
+var lastMonth = new Date(
+  today.getFullYear(),
+  today.getMonth(), 0
+).getDate();
 var presentDay = new Date().getDate();
 document.getElementById("today-date").innerHTML = today.toLocaleString('default', { month: 'long' });
 var todays = new Date();
 document.getElementById("today-year").innerHTML = todays.getFullYear();
 var days = "";
-for (let j = preDays; j < 0; j--) {
-  days += "<div class='next-date'>" + (lastEndDate - j + 1) + "</div>"
+for (let j = preDays; j > 0; j--) {
+  days += "<div class='next-date'>" + (lastMonth - j + 1) + "</div>"
 }
 for (let i = 1; i <= endDate; i++) {
   if (i == presentDay) {
